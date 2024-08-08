b1997118d99c9684d06a1625f2492009f0638e0b
“AleksanderKhatkov9” <alekskhatkov91@gmail.com>
2024-08-08 21:44:07 +0300

    init: start human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    // return number.toString();
+    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+
+    if (number === 0) {
+        return ones[0];
+    }
+
+    let result = '';
+
+    if (number >= 100) {
+        result += ones[Math.floor(number / 100)] + ' hundred';
+        number %= 100;
+        if (number > 0) result += ' ';
+    }
+
+    if (number >= 20) {
+        result += tens[Math.floor(number / 10)];
+        number %= 10;
+        if (number > 0) result += ' ' + ones[number];
+    } else if (number >= 10) {
+        result += teens[number - 10];
+    } else if (number > 0) {
+        result += ones[number];
+    }
+
+    return result;
 }
